d118b9bb3242188e47ca3cd4100d08b37fe3eda2
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-11 19:18:59 +0800

    Delete app directory

--- app/activity/activity-module.js
+++
@@ -1,8 +0,0 @@
-angular.module('ushahidi.activity', [])
-
-.config(require('./activity-routes.js'))
-
-.directive('activityTimeline', require('./activity-timeline.directive.js'))
-.directive('activityBarChart', require('./bar-chart.directive.js'))
-.directive('activityTimeChart', require('./time-chart.directive.js'))
-;
